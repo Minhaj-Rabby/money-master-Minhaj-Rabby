387df141c94325407258909be70b4e841f4cc2f2
Minhaj <minhaj12113@gmail.com>
2022-02-17 19:14:39 +0600

    Balance and Expence calculation completed

--- script/calculation.js
+++ script/calculation.js
@@ -1,20 +1,40 @@
+//getting input function
+
 function getInput(input) {
     const inputField = document.getElementById(input + '-money');
     return inputField;
 }
+
+//calculation function
+
 function calculate(income,food,rent,clothes){
+
+    //getting expense field
     const totalExpenseField = document.getElementById('total-expenses');
+
+    //calculating expense
     const totalExpense = parseFloat(food.value) + parseFloat(rent.value) + parseFloat(clothes.value);
 
+    //setting expense value
     totalExpenseField.innerText=totalExpense;
-    console.log(totalExpense);
-}
 
+    //getting balance field
+    const balanceField = document.getElementById('balance');
+
+    //setting balance
+    const balance= parseFloat(income.value)-totalExpense;
+    balanceField.innerText=balance;
+
+}
+//event on calculate button
 document.getElementById('calculate-btn').addEventListener('click',function(){
+    //getting input
    const incomeMoney= getInput('income');
    const foodMoney= getInput('food');
    const rentMoney= getInput('rent');
    const clothesMoney= getInput('clothes');
+
+   //calculate
    calculate(incomeMoney,foodMoney,rentMoney,clothesMoney);
 
 })
