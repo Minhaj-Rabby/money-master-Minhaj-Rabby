f0d1b313c958f755e1e60d15fce28f641ddc5aa2
Minhaj <minhaj12113@gmail.com>
2022-02-17 20:35:01 +0600

    Save function Completed

--- script/calculation.js
+++ script/calculation.js
@@ -2,10 +2,13 @@
 
 function getInput(input) {
     const inputField = document.getElementById(input + '-money');
-    return inputField;
+    const value = parseFloat(inputField.value);
+    inputField.value = '';
+    return value;
 }
 
 //calculation function
+let balance;
 
 function calculate(income,food,rent,clothes){
 
@@ -13,8 +16,8 @@ function calculate(income,food,rent,clothes){
     const totalExpenseField = document.getElementById('total-expenses');
 
     //calculating expense
-    const totalExpense = parseFloat(food.value) + parseFloat(rent.value) + parseFloat(clothes.value);
-
+    
+    const totalExpense = food + rent+ clothes;
     //setting expense value
     totalExpenseField.innerText=totalExpense;
 
@@ -22,14 +25,15 @@ function calculate(income,food,rent,clothes){
     const balanceField = document.getElementById('balance');
 
     //setting balance
-    const balance= parseFloat(income.value)-totalExpense;
+    balance= income-totalExpense;
     balanceField.innerText=balance;
 
 }
+let incomeMoney;
 //event on calculate button
 document.getElementById('calculate-btn').addEventListener('click',function(){
     //getting input
-   const incomeMoney= getInput('income');
+   incomeMoney= getInput('income');
    const foodMoney= getInput('food');
    const rentMoney= getInput('rent');
    const clothesMoney= getInput('clothes');
@@ -37,6 +41,18 @@ document.getElementById('calculate-btn').addEventListener('click',function(){
    //calculate
    calculate(incomeMoney,foodMoney,rentMoney,clothesMoney);
 
+})
+document.getElementById('save-button').addEventListener('click',function(){
+   
+    const saveMoney = getInput('save');
+    const savingField = document.getElementById('saving-amount');
+    const saveBalance = (saveMoney * incomeMoney)/ 100.0;
+    savingField.innerText = saveBalance;
+
+    const RemainingBalanceField = document.getElementById('remaining-balance');
+
+    const remainingBalance=balance-saveBalance;
+    RemainingBalanceField.innerText=remainingBalance;
 })
 
 
